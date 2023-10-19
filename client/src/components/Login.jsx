1a1861fa5d5f0605f23ccdb277b207537ec3638a
miqela97 <132955511+miqela97@users.noreply.github.com>
2023-10-19 15:45:43 -0400

    Update Login.jsx

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+function Login({ attemptLogin }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleChangeUsername = (e) => setUsername(e.target.value);
+    const handleChangePassword = (e) => setPassword(e.target.value);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+
+        const user = { username, password };
+
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                attemptLogin(data);
+            })
+            .catch((error) => {
+                console.error("Error logging in:", error);
+            });
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h2>Login</h2>
+
+                <input
+                    type="text"
+                    onChange={handleChangeUsername}
+                    value={username}
+                    placeholder="username"
+                />
+
+                <input
+                    type="password"
+                    onChange={handleChangePassword}
+                    value={password}
+                    placeholder="password"
+                />
+
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+    );
+}
+
+export default Login;
