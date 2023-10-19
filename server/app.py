e204c8212b566de9bcfb0d5d881444c8012857de
Chris Leary <learymchris@gmail.com>
2023-10-19 14:22:10 -0400

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,21 +1,149 @@
 #!/usr/bin/env python3
-
+from flask import Flask, request, make_response, jsonify,session
+from flask_cors import CORS
+from flask_migrate import Migrate
 # Standard library imports
 
 # Remote library imports
 from flask import request
 from flask_restful import Resource
+from flask_bcrypt import Bcrypt
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import db, User, Order, Product, OrderProduct
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"
+
+bcrypt = Bcrypt(app)
+
+CORS(app)
+migrate = Migrate(app, db)
+
+db.init_app(app)
 
 # Views go here!
 
 @app.route('/')
 def index():
-    return '<h1>Project Server</h1>'
+    return 'the stuff'
+
+# @app.post("/users")
+# def create_user():
+#     json = request.json
+
+#     password_digest = bcrypt.generate_password_hash(json["password"]).decode("utf-8")
+
+#     new_user = User(username=json["username"], password_digest=password_digest)
+#     db.session.add(new_user)
+#     db.session.commit()
+#     session["user_id"] = new_user.id
+#     return new_user.to_dict(), 201
+
+
+# SESSION LOGIN/LOGOUT#
+
+
+# @app.post("/login")
+# def login():
+#     json = request.json
+#     user = User.query.filter(User.username == json["username"]).first()
+
+#     if user and bcrypt.check_password_hash(user.password_digest, json["password"]):
+#         session["user_id"] = user.id
+#         return user.to_dict(), 200
+#     else:
+#         return {"error": "Invalid username or password"}, 401
+
+
+# @app.get("/check_session")
+# def check_session():
+#     user = User.query.filter(User.id == session.get("user_id")).first()
+#     if user:
+#         return user.to_dict(), 200
+#     else:
+#         return {"message": "No user logged in"}, 401
+
+
+# @app.delete("/logout")
+# def logout():
+#     session.pop("user_id")
+#     return {"message": "Logged out"}, 200
+
+
+@app.get("/users")
+def get_users():
+    users = User.query.all()
+    data = [user.to_dict(rules=("-orders",)) for user in users]
+    return make_response(jsonify(data), 200)
+
+
+@app.get("/users/<int:id>")
+def get_user_by_id(id):
+    user = User.query.filter(User.id == id).first()
+    if not user:
+        make_response(jsonify({"error": "no id matches that"}), 404)
+    user_dict = user.to_dict()
+    return make_response(jsonify(user_dict), 200)
+
+
+
+@app.get("/orders")
+def get_cart():
+    order = Order.query.filter(Order.id == session.get("user_id")).first()
+    if db.session.get(User, session.get("user_id")):
+        return {"total": order.total_price()}, 200
+    else:
+        return {"error": "not authorized"}, 401
+
+
+@app.get("/products/<int:id>")
+def get_products(id):
+    product = db.session.get(Product, id)
+    users = [d.to_dict(rules=("-carts",)) for d in product.users]
+    product_dict = product.to_dict(rules=("-carts",))
+    product_dict["users"] = users
+    return make_response(jsonify(product_dict), 200)
+
+@app.patch("/products/<int:id>")
+def patch_products(id):
+    data = request.get_json()
+    product = Product.query.filter(Product.id == id).first()
+    if not product:
+        make_response(jsonify({"error": "no such product"}), 404)
+    try:
+        for key in data:
+            setattr(product, key, data[key])
+        db.session.add(product)
+        db.session.commit()
+        return make_response(jsonify(product.to_dict()), 201)
+    except:
+        return make_response(jsonify({"error": "could not update product"}), 405)
+
+
+@app.post("/carts")
+def post_cart():
+    data = request.json
+    try:
+        cart = Cart(
+            product_id=data.get("product_id"),
+            user_id=data.get("user_id"),
+            day=data.get("day"),
+        )
+        db.session.add(cart)
+        db.session.commit()
+        return make_response(
+            jsonify(cart.to_dict(rules=("-product_id", "-user_id"))), 201
+        )
+    except Exception as e:
+        return make_response(jsonify({"error": str(e)}), 405)
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,6 +1,91 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
+
 # Models go here!
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class User(db.Model):
+    __tablename__ = "users_table"
+
+    # serialize_rules = ("-order_list.product_object",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    #password_digest = db.Column(db.String)
+
+    order_list = db.relationship("Order", back_populates="users", cascade="all, delete-orphan")
+
+    # def __repr__(self):
+    #     return f'<User id="{self.id}" username="{self.username}">'
+
+    # def to_dict(self):
+    #     return {"id": self.id, "username": self.username}
+
+
+class Order(db.Model, SerializerMixin):
+    __tablename__ = "order_table"
+
+    serialize_rules = ("-order_products", "-users")
+
+    id = db.Column(db.Integer, primary_key=True)
+    total_price = db.Column(db.Float)
+    closed = db.Column(db.Boolean, default = False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users_table.id"))
+
+    users = db.relationship("User", back_populates="order_list")
+    
+    order_products = db.relationship("OrderProduct", back_populates="order_object")
+    products = association_proxy("order_products","product_object")
+    
+
+    # def to_dict(self):
+    #     return {"id": self.id,"total_price": self.total_price}
+    
+    def total_price(self):                                          
+        sum_price = sum([x.price for x in self.products])            
+        return (sum_price)                                            
+        
+class OrderProduct(db.Model, SerializerMixin):
+    __tablename__ = "orderproduct_table"
+
+    # serialize_rules = ("-order_object.order_products", "-product_object.order_products")
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey("order_table.id"))
+    product_id = db.Column(db.Integer, db.ForeignKey("product_table.id"))
+
+    order_object = db.relationship("Order", back_populates = "order_products")
+    product_object = db.relationship("Product", back_populates = "order_products")
+
+
+
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = "product_table"
+
+    # serialize_rules = ("-order_products.product_object",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float)
+    image_url = db.Column(db.String, nullable = False)
+
+    order_products = db.relationship("OrderProduct", back_populates="product_object")
+    orders = association_proxy("order_products", "order_object")
+
+
+    # def to_dict(self):
+    #     return {"id": self.id, "name": self.name, "price": self.price, "image_url": self.image_url}
