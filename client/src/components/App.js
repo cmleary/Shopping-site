04e50b077e1a40f50962b9352b76d2692115091b
miqela97 <132955511+miqela97@users.noreply.github.com>
2023-10-19 15:45:17 -0400

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import LogIn from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import StoreList from "./StoreList";
+import Signup from "./Signup";
 
 function App() {
-  return <h1>Project Client</h1>;
+  const [cart, setCart] = useState([]);
+  const [allprod, setAllprod] = useState([]);
+
+  useEffect (() => {
+    fetch('/products')
+    .then(res => res.json())
+    .then(carsData => setAllprod(carsData))
+  },[])
+
+  function addToCart(item) {
+    setCart((prevCart) => [...prevCart, item]);
+  }
+
+  const removeItemFromCart = (itemId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== itemId));
+  }
+
+  return (
+    <div>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Log In</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/shopping-cart">Shopping Cart</Link>
+            </li>
+            <li>
+              <Link to="/store-list">Store List</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<LogIn  />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/shopping-cart" element={<ShoppingCart cart={cart} onRemoveItem={removeItemFromCart} />} />
+          <Route path="/store-list" element={<StoreList cart={cart} addToCart={addToCart} allprod={allprod}/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
